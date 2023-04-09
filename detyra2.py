ee8bb3a28ab54643a9d464aa4579a6e75fdbaa88
Roni <roni.kukaj@student.uni-pr.edu>
2023-04-09 13:52:29 +0200

    ndryshim

--- detyra2.py
+++ detyra2.py
@@ -27,8 +27,6 @@ def generateKey():
     return key
 
 
-
-
 # definimi i shkronjave dhe korrespondenteve numra te tyre
 alphabet = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6,
             'G': 7, 'H': 8, 'I': 9, 'J': 10, 'K': 11, 'L': 12,
