ed47baa5942e259179b5ce4b32554e591590a773
Roni <roni.kukaj@student.uni-pr.edu>
2023-04-09 14:19:19 +0200

    + plain to number

--- Encryption.py
+++ Encryption.py
@@ -4,12 +4,14 @@ import string
 
 class Encryption:
 
-    def generate_string_seed(self, gjatesia):
+    @staticmethod
+    def generate_string_seed(gjatesia):
         alphabet = string.ascii_letters + string.digits
         seed = ''.join(secrets.choice(alphabet) for i in range(gjatesia)).upper()
         return seed
 
-    def xor(self,a, b):
+    @staticmethod
+    def xor(a, b):
         if a == '1' and b == '1':
             return '0'
         elif a == '1' and b == '0':
@@ -18,6 +20,8 @@ class Encryption:
             return '1'
         elif a == '0' and b == '0':
             return '0'
+
+    @staticmethod
     def binary_to_decimal(binary_string):
         decimal_value = 0
         binary_string = str(binary_string)
@@ -25,8 +29,15 @@ class Encryption:
             decimal_value = decimal_value * 2 + int(digit)
         return decimal_value
 
-
+    @staticmethod
     def decimal_to_binary(decimal_num):
         binary_num = bin(decimal_num)
         # bin() returns a string with '0b' prefix, so we slice it off using [2:]
         return binary_num[2:]
+
+    @staticmethod
+    def plain_to_number(plaintext):
+        number_message = list()
+        for i in range(0, len(plaintext)):
+            number_message.append(ord(plaintext[i]))
+        return number_message
