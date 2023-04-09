ddc4b834c1de9fd8f002b5e7dc53d616bfc7b6e5
Suhejl1 <121341827+Suhejl1@users.noreply.github.com>
2023-04-09 14:07:13 +0200

    +xor

--- Encryption.py
+++ Encryption.py
@@ -9,3 +9,13 @@ class Encryption:
         seed = ''.join(secrets.choice(alphabet) for i in range(gjatesia)).upper()
         return seed
 
+    def xor(a, b):
+        if a == '1' and b == '1':
+            return '0'
+        elif a == '1' and b == '0':
+            return '1'
+        elif a == '0' and b == '1':
+            return '1'
+        elif a == '0' and b == '0':
+            return '0'
+
